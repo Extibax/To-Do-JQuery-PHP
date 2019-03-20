620d25d099ce6fb05435e52cd94f3ae270d9b694
Extibax <extibax@gmail.com>
2019-03-20 00:14:55 -0500

    Adding inserts tests to the DB

---
+++ sql/insert_testdata.sql
@@ -0,0 +1,14 @@
+USE to_do_extibaxinc;
+
+/* User to test DB */
+INSERT INTO users VALUES (1, 'extibax', 'extibax@gmail.com', 'sebas20', NULL);
+
+/* Category to test DB */
+INSERT INTO categories VALUES (1, 'inbox');
+
+/* Todos to test DB */
+INSERT INTO todos VALUES (1, 1, 1, 'Terminar los proyectos', NULL, NULL);
+
+INSERT INTO todos VALUES (2, 1, 1, 'Continuar con el TODO Project', NULL, NULL);
+
+INSERT INTO todos VALUES (3, 1, 1, 'Thank GOD for this gift', NULL, NULL);
