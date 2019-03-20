e6e3a2fdacb114c60352fb603647776b2a70de8d
Extibax <extibax@gmail.com>
2019-03-20 00:03:33 -0500

    Advances in the edit function

--- js/App.js
+++ js/App.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 
     fetchTodos();
 
-    $('#form-todo').keypress(function (e) {
+    $('#form-save-todo').keypress(function (e) {
         var code = (e.keyCode ? e.keyCode : e.which);
         if (code == 13) {
             e.preventDefault();
@@ -10,33 +10,75 @@ $(document).ready(() => {
             fetchTodos();
         }
     });
+
+    $('#form-edit-todo').keypress(function (e) {
+        var code = (e.keyCode ? e.KeyCode : e.which);
+        if (code == 13) {
+            e.preventDefault();
+            let element = $(this)[0];
+            console.log(element);
+        }
+    });
     
 });
 
-function saveTodo() {
-    const postTodo = {
+function saveTodo()
+{
+    const postTodoSave = {
         Todo: $('#input-todo').val()
     };
 
-    $.post('./php/save_todo.php', postTodo, (response) => {
-        $('#form-todo').trigger('reset');
+    $.post('./php/save_todo.php', postTodoSave, (response) => {
+        $('#form-save-todo').trigger('reset');
         fetchTodos();
         console.log('Save todo: ' + response);
     });
 }
 
-function fetchTodos() {
+function fetchTodos()
+{
     $.get('./php/fetch_todos.php', 'aplication/json', (response) => {
         let todos = JSON.parse(response);
         let template = '';
 
         todos.forEach(todo => {
             template +=
-                `<div class="list-group-item bg-dark mb-1 d-flex justify-content-between align-items-center">
-                ${todo.Todo} <span class="badge badge-primary badge-pill">${todo.Date}</span>
-            </div>`;
+            `
+            <div class="list-group-item bg-dark mb-1" todo-id = "${todo.ID}">
+            <div class="d-flex justify-content-between align-items-center">
+                <span class="d-flex flex-column">
+                    <span>${todo.Todo}</span>
+                    <span class="badge badge-primary badge-pill">${todo.Date}</span>
+                </span>
+                <span>
+                    <button class="btn btn-primary" id="edit-todo" type="button" data-toggle="collapse"
+                        data-target="#edit-menu-${todo.ID}" aria-expanded="false" aria-controls="edit-menu-${todo.ID}">
+                        <i class="far fa-edit"></i>
+                    </button>
+                    <button class="btn btn-success" id="check-todo">
+                        <i class="far fa-check-square"></i>
+                    </button>
+                </span>
+            </div>
+            <div class="collapse" id="edit-menu-${todo.ID}">
+                <form class="form-inline form-group align-self-center w-100 mx-0 my-2" id="form-edit-todo">
+                    <div class="input-group w-100">
+                        <input class="form-control bg-dark border-primary w-100 text-white" id="input-edit-todo" value='${todo.Todo}'
+                            type="text" placeholder="Edit your todo here">
+                    </div>
+                </form>
+            </div>
+        </div>
+            `;
         });
 
         $('#todos').html(template);
     })
+}
+
+function editTodo()
+{
+    /* postTodoEdit = {
+        Todo
+    } */
 }
