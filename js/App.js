e1e5f826869a07acc7e63cc7424426c1844b0cf5
Extibax <extibax@gmail.com>
2019-03-20 19:31:18 -0500

    Advances in the Edit func

--- js/App.js
+++ js/App.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
 
     $('#form-save-todo').keypress(function (e) {
         var code = (e.keyCode ? e.keyCode : e.which);
+        console.log(code);
         if (code == 13) {
             e.preventDefault();
             saveTodo();
@@ -11,23 +12,20 @@ $(document).ready(() => {
         }
     });
 
-    $('#form-edit-todo').keypress(function (e) {
-        e.preventDefault();
-        var code = (e.keyCode ? e.KeyCode : e.which);
-        console.log(code);
+    $(document).on('keypress', '#input-edit-todo', function (e) {
+        let code = (e.KeyCode ? e.KeyCode : e.which);
         if (code == 13) {
-            $('#form-edit-todo').trigger('reset');
-            
+            let input_edit = $(this)[0];
+            let input_edit_value = $(input_edit).val();
 
-            let element = $(this)[0];
-            console.log(element);
+            //TODO: Continuar con el envio del dato editado a la DB
+            $.post();
         }
     });
-    
+
 });
 
-function saveTodo()
-{
+function saveTodo() {
     const postTodoSave = {
         Todo: $('#input-todo').val()
     };
@@ -39,50 +37,47 @@ function saveTodo()
     });
 }
 
-function fetchTodos()
-{
+function fetchTodos() {
     $.get('./php/fetch_todos.php', 'aplication/json', (response) => {
         let todos = JSON.parse(response);
         let template = '';
 
         todos.forEach(todo => {
             template +=
-            `
-            <div class="list-group-item bg-dark mb-1" todo-id = "${todo.ID}">
-            <div class="d-flex justify-content-between align-items-center">
-                <span class="d-flex flex-column">
-                    <span>${todo.Todo}</span>
-                    <span class="badge badge-primary badge-pill">${todo.Date}</span>
-                </span>
-                <span>
-                    <button class="btn btn-primary" id="edit-todo" type="button" data-toggle="collapse"
-                        data-target="#edit-menu-${todo.ID}" aria-expanded="false" aria-controls="edit-menu-${todo.ID}">
-                        <i class="far fa-edit"></i>
-                    </button>
-                    <button class="btn btn-success" id="check-todo">
-                        <i class="far fa-check-square"></i>
-                    </button>
-                </span>
+                `
+                <div class="list-group-item bg-dark mb-1" todo-id="${todo.ID}">
+                <div class="d-flex justify-content-between align-items-center">
+                    <span class="d-flex flex-column">
+                        <span>${todo.Todo}</span>
+                        <span class="badge badge-primary badge-pill">${todo.Date}</span>
+                    </span>
+                    <span>
+                        <button class="btn btn-primary" id="btn-edit-todo" type="button" data-toggle="collapse"
+                            data-target="#edit-menu-${todo.ID}" aria-expanded="false" aria-controls="edit-menu-${todo.ID}">
+                            <i class="far fa-edit"></i>
+                        </button>
+                        <button class="btn btn-success" id="check-todo">
+                            <i class="far fa-check-square"></i>
+                        </button>
+                    </span>
+                </div>
+                <div class="collapse" id="edit-menu-${todo.ID}">
+                    <form class="form-inline form-group w-100 mx-0 my-2" id="form-edit-todo">
+                        <div class="input-group w-100">
+                            <input class="form-control bg-dark border-primary w-100 text-white" id="input-edit-todo"
+                                value='${todo.Todo}' type="text" placeholder="Edit your todo here">
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="collapse" id="edit-menu-${todo.ID}">
-                <form class="form-inline form-group align-self-center w-100 mx-0 my-2" id="form-edit-todo">
-                    <div class="input-group w-100">
-                        <input class="form-control bg-dark border-primary w-100 text-white" id="input-edit-todo" value='${todo.Todo}'
-                            type="text" placeholder="Edit your todo here">
-                    </div>
-                </form>
-            </div>
-        </div>
             `;
         });
 
         $('#todos').html(template);
+
     })
 }
 
-function editTodo()
-{
-    /* postTodoEdit = {
-        Todo
-    } */
+function editTodo() {
+
 }
