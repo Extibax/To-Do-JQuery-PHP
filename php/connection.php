afe77b0cebc5c5d3c395dd942ec47f8434dae7a2
Extibax <extibax@gmail.com>
2019-03-22 16:12:57 -0500

    DB now is OK with env variables

--- php/connection.php
+++ php/connection.php
@@ -1,18 +1,15 @@
 <?php
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-
 $main_path = dirname(__DIR__);
 
 $dotenv = Dotenv\Dotenv::create($main_path);
 $dotenv->load();
 
-echo $_ENV['NAME'];
-
-$HOST = "localhost";
-$USER = "root";
-$PASS = "";
-$DB = "to_do_extibaxinc";
+$HOST = $_ENV['HOST'];
+$USER = $_ENV['USER'];
+$PASS = $_ENV['PASS'];
+$DB = $_ENV['DB'];
 
 $connection = mysqli_connect(
     $HOST,
